65391c8cdad593081299b8c62de1d0a738e502d5
January <januaryobrien.ux@gmail.com>
2020-11-25 13:51:58 -0500

    test branch

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Test!</p>
+        <p>Test 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
