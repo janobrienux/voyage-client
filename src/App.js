3959c5b612e6f1b374dbb7bc16f598a59464996b
Laura Blitz <laura.k.blitz@gmail.com>
2020-11-28 11:54:04 -0500

    Edit Voyages in progress

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 
 function App() {
   const [token, setToken] = useState(undefined)
+  // const [token, setToken] = useState('')
+
 
   const viewConductor =()=>{
     return token === undefined ? 'Ready to Voyage?' : <VoyagesSplashPage />
@@ -12,27 +14,8 @@ function App() {
 
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Test!</p>
-        <p>Test 2</p>
-        <p>Jake's TEST!!!</p>
-        <p>Laura's Branch</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <div className='JakesTestDiv'>
-        {/* <VoyagesSplashPage /> */}
+         <VoyagesSplashPage /> 
         {viewConductor()}
       </div>
   );
