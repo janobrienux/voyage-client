1be01857f0afddae40ee6d62fd0a06f058bddcd6
January <januaryobrien.ux@gmail.com>
2020-11-30 09:31:17 -0500

    small updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {useState} from 'react';
 import './App.css';
-// import Auth from './components/auth/Auth'
+ import Auth from './components/auth/Auth'
 import VoyagesSplashPage from './components/voyage/Voyages/VoyagesSplashPage';
 
 function App() {
